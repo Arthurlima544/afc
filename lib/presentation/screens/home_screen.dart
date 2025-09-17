d58282bc3c75c3552fa99ed9e66ff9b0f23a4e17
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-09-17 17:54:27 -0300

    feat: Circular Loading

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const Placeholder();
+  Widget build(BuildContext context) =>
+      const Center(child: CircularProgressIndicator(size: 25));
 }
