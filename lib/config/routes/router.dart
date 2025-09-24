c2d5c2c0db751d6cba96480fa894566703ee8c0e
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-09-24 08:01:35 -0300

    feat: adding routers path

--- lib/config/routes/router.dart
+++ lib/config/routes/router.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../presentation/blocs/category/category_cubit.dart';
+import '../../presentation/blocs/transaction/transaction_cubit.dart';
+import '../../presentation/screens/cadastrar_categoria.dart';
+import '../../presentation/screens/cadastrar_transacao.dart';
 import '../../presentation/screens/home_screen.dart';
 import '../../presentation/screens/login_screen.dart';
 
@@ -18,6 +23,25 @@ final GoRouter router = GoRouter(
         publishableKey:
             'pk_test_Y3VycmVudC1waWdsZXQtMTYuY2xlcmsuYWNjb3VudHMuZGV2JA',
       ),
+    ),
+
+    GoRoute(
+      path: '/cadastro-categoria',
+      builder: (BuildContext context, GoRouterState state) =>
+          BlocProvider<CategoryCubit>(
+            create: (BuildContext context) => CategoryCubit(),
+            child: const CadastrarCategoria(),
+          ),
+    ),
+
+    GoRoute(
+      path: '/cadastro-transacao',
+      builder: (BuildContext context, GoRouterState state) =>
+          BlocProvider<TransactionCubit>(
+            create: (BuildContext context) =>
+                TransactionCubit()..getCategories(),
+            child: const CadastrarTransacao(),
+          ),
     ),
   ],
 );
